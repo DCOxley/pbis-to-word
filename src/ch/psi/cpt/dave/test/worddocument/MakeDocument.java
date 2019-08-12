95dd4dcf8f7263337b61bf2839b395be44696489
oxleyd <oxleyd@pc11450.psi.ch>
2019-08-12 15:55:47 +0200

    remove pbi model. not needed

--- src/ch/psi/cpt/dave/test/worddocument/MakeDocument.java
+++ src/ch/psi/cpt/dave/test/worddocument/MakeDocument.java
@@ -2,13 +2,14 @@ package ch.psi.cpt.dave.test.worddocument;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.List;
 
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
 
 import ch.psi.cpt.dave.test.worddocument.io.PBIReportReader;
-import ch.psi.cpt.dave.test.worddocument.model.PBIModel;
+import ch.psi.cpt.dave.test.worddocument.model.PBITask;
 import ch.psi.cpt.dave.test.worddocument.model.util.XmlToPBIAdapter;
 import ch.psi.cpt.dave.test.worddocument.model.xml.Root;
 
@@ -30,11 +31,11 @@ public class MakeDocument {
 		XWPFRun run = paragraph.createRun();
 		run.setText("Hello, This is javatpoint. This paragraph is written " + "by using XWPFParagrah.");
 
-		PBIReportReader reader = new PBIReportReader(new File(INPUT_RESOURCE_DIRECTORY+"Technical.xml"));
+		PBIReportReader reader = new PBIReportReader(new File(INPUT_RESOURCE_DIRECTORY+"TechnicalFull.xml"));
 		Root root = reader.read();
 		
 		XmlToPBIAdapter adapter = new XmlToPBIAdapter();
-		PBIModel model = adapter.construct(root);
+		List<PBITask> pbiTasks = adapter.construct(root);
 		
 		// Write the Document in file system
 		FileOutputStream out = new FileOutputStream(new File(OUTPUT_RESOURCE_DIRECTORY+"createdocument.docx"));
--- src/ch/psi/cpt/dave/test/worddocument/model/util/XmlToPBIAdapter.java
+++ src/ch/psi/cpt/dave/test/worddocument/model/util/XmlToPBIAdapter.java
@@ -1,22 +1,24 @@
 package ch.psi.cpt.dave.test.worddocument.model.util;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import ch.psi.cpt.dave.test.worddocument.model.PBIModel;
+import ch.psi.cpt.dave.test.worddocument.model.PBITask;
 import ch.psi.cpt.dave.test.worddocument.model.xml.Root;
 import ch.psi.cpt.dave.test.worddocument.model.xml.Task;
 
 public class XmlToPBIAdapter {
 	
-	public PBIModel construct(Root xmlExportedRoot) {
-		PBIModel resultModel = new PBIModel();
+	public List<PBITask> construct(Root xmlExportedRoot) {
+		List<PBITask> resultPBIs = new ArrayList<PBITask>();
 		
 		List<Task> tasks = xmlExportedRoot.getTracker().getTasks();
+		List<PBITask> pBIs = new ArrayList<PBITask>();
 		for (Task task : tasks) {
 			System.out.println(task.getId());
 		}
 		
-		return resultModel;
+		return resultPBIs;
 	}
 	
 }
