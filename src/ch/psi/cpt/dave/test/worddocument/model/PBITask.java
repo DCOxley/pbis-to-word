18806ab56be436b326916e055f6f225fb03303dd
oxleyd <oxleyd@pc11450.psi.ch>
2019-08-12 15:55:59 +0200

    add pbi task to model

---
+++ src/ch/psi/cpt/dave/test/worddocument/model/PBITask.java
@@ -0,0 +1,16 @@
+package ch.psi.cpt.dave.test.worddocument.model;
+
+public class PBITask {
+	private String parent; 
+	private String priority;
+	private String summary;
+	private String status;
+	private String severity;
+	private String resolution;
+	private String category;
+	private String sprint;  
+	
+	
+	
+	
+}
